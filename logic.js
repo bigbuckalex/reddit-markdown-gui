10ae8fc8e883b158d2e565513d2dbe608bb9cd4a
Alex Lilly <bigbuckalex@gmail.com>
2022-03-08 20:52:08 -0800

    added bold function (backspace still needs fixed)

--- logic.js
+++ logic.js
@@ -4,33 +4,62 @@ let isStrike = false;
 let isSuper = false;
 
 function updateMarkdown(e) {
-    console.log(e);
+
+    let markdown = document.getElementById("markdown");
+
+    console.log(e); 
     // alphanumeric character
     if (e.inputType == "insertText") {
-        document.getElementById("markdown").value += e.data;
+        if (isItalic && isBold) {
+            markdown.value = markdown.value.substring(0, markdown.value.length - 3) + e.data + markdown.value.substring(markdown.value.length - 3);
+        }
+        else if (isItalic) {
+            markdown.value = markdown.value.substring(0, markdown.value.length - 1) + e.data + markdown.value.substring(markdown.value.length - 1);
+        }
+        else if (isBold) {
+            markdown.value = markdown.value.substring(0, markdown.value.length - 2) + e.data + markdown.value.substring(markdown.value.length - 2);
+        }
+        else
+            markdown.value += e.data;
     }
     // backspace
     else if (e.inputType == "deleteContentBackward") {
-        document.getElementById("markdown").value = document.getElementById("markdown").value.slice(0, -1);
+        markdown.value = markdown.value.slice(0, -1);
     }
     // enter
     else if (e.inputType == "insertLineBreak") {
-        document.getElementById("markdown").value += "\n";
+        markdown.value += "\n";
     }
 }
 
 function toggleItalic(e) {
+    let markdown = document.getElementById("markdown");
     e.preventDefault();
     // turning italic on
     if (!isItalic) {
         console.log("italic on");
         isItalic = true;
-        document.getElementById("markdown").value += "*";
+        markdown.value += "**";
     }
     // turning italic off
     else {
         console.log("italic off");
         isItalic = false;
-        document.getElementById("markdown").value += "*";
+    }
+}
+
+function toggleBold(e) {
+    let markdown = document.getElementById("markdown");
+    e.preventDefault();
+    // turning bold on
+    if (!isBold) {
+        console.log("bold on");
+        isBold = true;
+        markdown.value += "****";
+    }
+    // turning bold off
+    else {
+        console.log("bold off");
+        isBold = false;
     }
 }
